a4293096c14dccf2ecf76fabf0d5796c7bf00704
Michael <mpeng@optimum.net>
2022-01-28 23:01:25 -0500

    worldpizza can flip the world

--- Assets/WorldPizza.cs
+++ Assets/WorldPizza.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WorldPizza : MonoBehaviour {
-    // Start is called before the first frame update
+    public float FlipDuration = 0.5f;
+
+    private float flipTime = 0;
+    private bool isFlipping = false;
+
     void Start() {
         
     }
 
     // Update is called once per frame
     void Update() {
-        
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            this.flipTime = 0;
+            this.isFlipping = true;
+        }
+
+        if (this.isFlipping) {
+            this.flipTime = Mathf.Min(this.flipTime + Time.deltaTime / FlipDuration, 1f);
+            this.gameObject.transform.Rotate(new Vector3(0f, 0f, Time.deltaTime * 180 / FlipDuration), Space.Self);
+            if (this.flipTime == 1f) {
+                this.isFlipping = false;
+                Transform transform = this.gameObject.transform;
+                transform.rotation = Quaternion.Euler(0f, 0f, Mathf.Round(transform.rotation.eulerAngles.z / 180f) * 180f,);
+            }
+        }
     }
 
     void Spin() {
