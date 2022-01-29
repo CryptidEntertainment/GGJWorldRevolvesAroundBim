7b04536a17d1392e422dbf54d5d64ca3e71338b6
ScottT4ylor <Scott.T4ylor@gmail.com>
2022-01-29 17:43:23 -0500

    Got world flipping tied in with game driver.

--- Assets/Code/WorldPizza.cs
+++ Assets/Code/WorldPizza.cs
@@ -8,14 +8,14 @@ public class WorldPizza : MonoBehaviour {
 
     private float flipTime = 0;
     private bool isFlipping = false;
-
+    private GameDriver gameDriver;
     void Start() {
-        
+        gameDriver = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameDriver>();
     }
 
     // Update is called once per frame
     void Update() {
-        if (!this.isFlipping && Input.GetKeyDown(KeyCode.Space)) {
+        if (!this.isFlipping && Input.GetAxis("Flip")!=0 && gameDriver.flip==true) {
             this.flipTime = 0;
             this.isFlipping = true;
             if (this.bim) this.bim.isKinematic = true;
