420a635535c26e272974260f9b24754c33c7deaa
Michael <mpeng@optimum.net>
2022-01-29 21:48:40 -0500

    cryptid text advances through mulitple pages

--- Assets/Code/Cryptid.cs
+++ Assets/Code/Cryptid.cs
@@ -2,29 +2,35 @@ using UnityEngine;
 
 public class Cryptid : MonoBehaviour {
     public GameObject helpText;
+    public GameObject[] texts;
 
-    // Start is called before the first frame update
-    void Start() {
-        
-    }
+    private float textTime = 0;
+    private float textStayDuration = 3;
 
-    // Update is called once per frame
-    void Update() {
-
+    private void Update() {
+        if (this.helpText && this.helpText.activeInHierarchy) {
+            this.textTime = Mathf.Min(this.textTime + Time.deltaTime / textStayDuration, this.texts.Length - 1);
+            this.texts[(int)Mathf.Max(0, this.textTime - 1)].SetActive(false);
+            this.texts[(int)this.textTime].SetActive(true);
+        }
     }
 
     void OnTriggerEnter2D(Collider2D collider) {
-        if (collider.tag == "Player")
-        {
+        if (collider.tag == "Player") {
             if (this.helpText) this.helpText.SetActive(true);
+            this.textTime = 0;
+            foreach (GameObject text in this.texts) {
+                if (text) text.SetActive(false);
+            }
         }
     }
 
     void OnTriggerExit2D(Collider2D collider) {
-        if (collider.tag == "Player")
-        {
-
+        if (collider.tag == "Player") {
             if (this.helpText) this.helpText.SetActive(false);
+            foreach (GameObject text in this.texts) {
+                if (text) text.SetActive(false);
+            }
         }
     }
 }
